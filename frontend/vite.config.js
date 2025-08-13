a7638d6e13ae3499fd8d6c3569c88eee23a47191
RitikaSharma003 <ritikasharma20032003@gmail.com>
2025-08-13 22:44:21 +0530

    vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   base: "/",
+  build: {
+    assetsDir: "assets", // Ensures assets go to /assets/
+  },
 });
